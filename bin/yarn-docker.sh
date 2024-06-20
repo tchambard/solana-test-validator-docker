12a7a305adf9fcf368b74ad3d9ca651125690131
t.chambard <t.chambard@bee-buzziness.com>
2024-06-20 16:00:47 +0200

    fix yarn-docker cli

--- bin/yarn-docker.sh
+++ bin/yarn-docker.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 'use strict';
-console.log("AAA")
-var shell = require('shelljsxxx');
+
+var shell = require('shelljs');
 
 if (shell.exec(`solana-docker-shell exec "yarn ${process.argv.slice(2).join(' ')}"`).code !== 0) {
   shell.echo('Error: yarn command failed');
